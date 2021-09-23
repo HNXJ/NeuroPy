bfdfbbaf79c5d2ad4543a8d2e5155ff6aeb5b1f3
Hamed Nejat <59973245+HNXJ@users.noreply.github.com>
2021-09-23 15:53:14 +0330

    Main testbench

---
+++ main.py
@@ -0,0 +1,16 @@
+import plotly.graph_objects as go
+import plotly.express as px
+import scipy.io as sio
+import numpy as np
+
+from Methods import *
+import plotly
+import cv2
+
+
+data = sio.loadmat('data.mat') # Example data, private access
+print_all_content(data)
+
+ERP_plot(save=True, data=data, key="pfc")
+CSD_plot(save=True, data=data, key='pfc')
+psp_plotter(data=data, key='pfc', save=True)
