554fdc4b09391f74ebf50b558f57333bb56fcec9
Hamed Nejat <59973245+HNXJ@users.noreply.github.com>
2021-10-06 01:38:25 +0330

    V1.5 + Selective trials

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from Methods import *
 # CSD_plot(save=True, data=data, key='pfc')
 
 ### Power spectrum (multitaper) plots on heatmap
-psp_plotter(data=data, key='pfc', save=True, t1=500, t2=2500, fmin=0,
-            fmax=100, normalize_w=True, bw=45, k=0, title="PS, all trials ")
+# psp_plotter(data=data, key='pfc', save=True, t1=500, t2=2500, fmin=0,
+#             fmax=100, normalize_w=True, bw=45, k=0, title="PS, all trials ")
 # psp_plotter(data=data, key='pfc', save=True, t1=2500, t2=4500, fmin=0,
 #             fmax=100, normalize_w=True, bw=45, k=0)
 
@@ -21,10 +21,19 @@ psp_plotter(data=data, key='pfc', save=True, t1=500, t2=2500, fmin=0,
 # coherence_plotter(data=data, key='pfc', save=True, t1=2500, t2=4500, fmin=4,
 #             fmax=100, normalize_w=True, bw=45, k=0)
 
-### Single trial parts
+### Selective or single trial parts: PSP and COH
+### PSP
 psp_plotter(data=data, key='pfc', save=True, t1=2500, t2=4500, fmin=0,
             fmax=100, normalize_w=True, bw=45, k=0, trials=[100],
             title="PS, trial 100")
 psp_plotter(data=data, key='pfc', save=True, t1=2500, t2=4500, fmin=0,
             fmax=100, normalize_w=True, bw=45, k=0, trials=[400],
             title="PS, trial 400")
+
+### COH
+coherence_plotter(data=data, key='pfc', save=True, t1=500, t2=2500, fmin=4,
+            fmax=100, normalize_w=True, bw=45, k=0,
+            title="Coherence, trial 100, 200", trials=[100, 200])
+coherence_plotter(data=data, key='pfc', save=True, t1=2500, t2=4500, fmin=4,
+            fmax=100, normalize_w=True, bw=45, k=0,
+            title="Coherence, trial 400, 500", trials=[400, 500])
