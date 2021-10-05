c9d03ce24e79559d64de19f7342fdb300e322da6
Hamed Nejat <59973245+HNXJ@users.noreply.github.com>
2021-10-05 13:09:34 +0330

    V1.3

--- Methods.py
+++ Methods.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import numpy as np
 
 import plotly
+import mne
 import cv2
 
 
@@ -98,19 +99,24 @@ def CSD_plot(save=False, data=None, key='pfc'):
     fig.show()
 
 
-def pspectlam(x, axis=0, fs=1000, fc=500, mode=None):
+def pspectlam(x, axis=0, fs=1000, fc=500, mode="MT", fmin=0, fmax=100, bw=15):
     if mode=="MT":
         
         ch_names = [str(i) for i in range(1, 17)]
         info = mne.create_info(ch_names=ch_names, sfreq=1000)
-        d = np.transpose(np.mean(data['lfp'][:, 0:16, :]), 2)
+        d = x
         
-        raw = mne.io.RawArray(d, info, first_samp=0, copy='auto', verbose=None)
-        f = mne.time_frequency.psd_multitaper(raw, fmin=0, fmax=fc, tmin=None, tmax=None,
-                                          bandwidth=2, adaptive=False, low_bias=True,
+        if len(d.shape) < 3:
+            raw = mne.io.RawArray(d, info, first_samp=0, copy='auto', verbose=None)
+        else:
+            raw = mne.EpochsArray(d, info, verbose=None)
+            
+        
+        # bw = 7
+        f = mne.time_frequency.psd_multitaper(raw, fmin=fmin, fmax=fmax, tmin=None, tmax=None,
+                                          bandwidth=bw, adaptive=False, low_bias=True,
                                           normalization='length', picks=ch_names, proj=False,
                                           n_jobs=1, verbose=None)
-        
         return f[0]
     
     x = x - np.mean(np.mean(x))
@@ -120,12 +126,14 @@ def pspectlam(x, axis=0, fs=1000, fc=500, mode=None):
     return _fft[:n, :, :]
 
 
-def pspectlamavg(x, axis=0, fs=1000, fc=1000):
-    xf = pspectlam(x, axis=axis, fs=fs, fc=fc)
-    return np.abs(np.mean(xf, 2))
+def pspectlamavg(x, axis=0, fs=1000, fc=1000, fmin=0, fmax=100, bw=15):
+    x = np.mean(x, 2)
+    xf = pspectlam(x.transpose(), axis=axis, fs=fs, fc=fc, fmin=fmin, fmax=fmax, bw=bw)
+    return xf.transpose()
 
 
-def psplot(x, save=True, show=True, filename="psp.html", w=300, h=200, t=None, y=None, relative=True):
+def psplot(x, save=True, show=True, filename="psp.html", w=300, h=200, t=None,
+           y=None, relative=True, reverse=True, title="PowerSpectrum"):
 
     im = x.transpose()
     if relative==True:
@@ -135,12 +143,17 @@ def psplot(x, save=True, show=True, filename="psp.html", w=300, h=200, t=None, y
     fig = go.Figure(
         data=go.Heatmap(z=im, y=y, x=t), 
         layout=go.Layout(
-            title="Power spectrum/ ",
+            title=title,
             xaxis=dict(title='Frequency(Hz)'),
             yaxis=dict(title='Channel ID')
         ),
     )
-
+    
+    if reverse==True: 
+        fig.update_layout(
+            yaxis = dict(autorange="reversed")
+        )
+        
     if save==True:
         plotly.offline.plot(fig, filename=filename)
     
@@ -183,7 +196,10 @@ def customplot(x, save=True, show=True, filename="plot.html"
     return
 
 
-def psp_plotter(data=None, key='pfc', save=False, t1=500, t2=2501):
+def psp_plotter(data=None, key='pfc', save=False,
+                t1=500, t2=2501, fmin=0, fmax=100,
+                normalize_w=False, k=5,
+                title="Power spectrum multitaper ", bw=15):
     
     lam1 = data['lfp'][:, 0:16, :]
     lam2 = data['lfp'][:, 16:32, :]
@@ -199,15 +215,82 @@ def psp_plotter(data=None, key='pfc', save=False, t1=500, t2=2501):
         print('Not in set')
         return
     
-    ps_pfc = pspectlamavg(Y, axis=0, fs=1000, fc=150)
+    ps_pfc = pspectlamavg(Y, axis=0, fs=1000, fc=150, fmin=fmin, fmax=fmax)
     
-    t = np.linspace(0, 150, 1000)
+    if normalize_w==True:
+        for i in range(ps_pfc.shape[0]-k):
+            if k > 0:
+                ps_pfc[i, :] /= np.max(np.max(ps_pfc[i:i+k, :]))
+            else:
+                ps_pfc[i, :] /= np.max(np.max(ps_pfc[i, :]))
+        if k > 0:
+            for i in range(ps_pfc.shape[0]-k, ps_pfc.shape[0]):
+                ps_pfc[i, :] /= np.max(np.max(ps_pfc[i, :]))
+        
+        title = title + ",Normalized relative to other channels"
+        
+    t = np.linspace(fmin, fmax, 1000)
     y = np.linspace(1, 16, 300)
         
-    psplot(ps_pfc, save=save, filename="ps_" + key + ".html", w=1000, h=300, t=t, y=y, relative=True)
+    psplot(ps_pfc, save=save, filename="ps_" + key + ".html", w=1000, h=300,
+           t=t, y=y, relative=True, title=title)
     return
     
 
+def coherence(x, y):
+
+    c = np.corrcoef(x, y)
+    return c[0, 1]
+
+
+def coherence_plotter(data=None, key='pfc', save=False,
+                t1=500, t2=2501, fmin=0, fmax=100,
+                normalize_w=False, k=5,
+                title="Spectral coherence multitaper ", bw=15):
     
+    lam1 = data['lfp'][:, 0:16, :]
+    lam2 = data['lfp'][:, 16:32, :]
+    lam3 = data['lfp'][:, 32:48, :]
     
+    if key=='pfc':
+        Y = lam1[t1:t2, :, :]
+    elif key=='p7a':
+        Y = lam2[t1:t2, :, :]
+    elif key=='v4':
+        Y = lam3[t1:t2, :, :]
+    else:
+        print('Not in set')
+        return
     
+    ps_pfc = pspectlamavg(Y, axis=0, fs=1000, fc=150, fmin=fmin, fmax=fmax)
+    
+    if normalize_w==True:
+        for i in range(ps_pfc.shape[0]-k):
+            if k > 0:
+                ps_pfc[i, :] /= np.max(np.max(ps_pfc[i:i+k, :]))
+            else:
+                ps_pfc[i, :] /= np.max(np.max(ps_pfc[i, :]))
+        if k > 0:
+            for i in range(ps_pfc.shape[0]-k, ps_pfc.shape[0]):
+                ps_pfc[i, :] /= np.max(np.max(ps_pfc[i, :]))
+        
+        title = title + ",Normalized relative to other channels"
+        
+    t = np.linspace(fmin, fmax, 1000)
+    y = np.linspace(1, 16, 300)
+    
+    c = np.zeros((ps_pfc.shape[1], ps_pfc.shape[1]))
+    for i in range(ps_pfc.shape[1]):
+        c[i, i] = 1
+        for j in range(i+1, ps_pfc.shape[1]):
+            u = coherence(ps_pfc[:, i], ps_pfc[:, j])
+            c[i, j] = u
+            c[j, i] = u
+            
+    t = np.linspace(1, 17, 16)
+    y = np.linspace(1, 17, 16)
+    customplot(c, save=True, show=True, filename="specCoherence.html"
+                   , w=16, h=16, t=t, y=y, relative=True
+                   ,xlabel="Ch ID", ylabel="Ch ID",
+                   title=title)
+    return
