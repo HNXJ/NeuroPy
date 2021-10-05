1a9452478084272aba24f930653ce084f5bcc57b
Hamed Nejat <59973245+HNXJ@users.noreply.github.com>
2021-10-05 13:09:47 +0330

    V1.3

--- main.py
+++ main.py
@@ -7,53 +7,39 @@ from Methods import *
 import plotly
 import cv2
 
-import mne
-from mne.time_frequency import tfr_morlet, psd_multitaper, psd_welch
-from mne.datasets import somato
-
 
 # data = sio.loadmat('data.mat') # Example data, private access
-print_all_content(data)
-ch_names = [str(i) for i in range(1, 17)]
-info = mne.create_info(ch_names=ch_names, sfreq=1000)
-d = np.transpose(data['lfp'][:, 0:16, 0])
+# print_all_content(data)
+# ch_names = [str(i) for i in range(1, 17)]
+# info = mne.create_info(ch_names=ch_names, sfreq=1000)
+# d = np.transpose(data['lfp'][:, 0:16, 0])
 
-raw = mne.io.RawArray(d, info, first_samp=0, copy='auto', verbose=None)
-f = mne.time_frequency.psd_multitaper(raw, fmin=4, fmax=36, tmin=None, tmax=None,
-                                  bandwidth=2, adaptive=False, low_bias=True,
-                                  normalization='length', picks=ch_names, proj=False,
-                                  n_jobs=1, verbose=None)
+# raw = mne.io.RawArray(d, info, first_samp=0, copy='auto', verbose=None)
+# f = mne.time_frequency.psd_multitaper(raw, fmin=4, fmax=36, tmin=None, tmax=None,
+#                                   bandwidth=2, adaptive=False, low_bias=True,
+#                                   normalization='length', picks=ch_names, proj=False,
+#                                   n_jobs=1, verbose=None)
 
-customplot(f[0].transpose(), save=True, show=True, filename="plot.html"
-               , w=300, h=200, t=np.linspace(4, 36, 300)
-               , y=np.linspace(1, 16, 200), relative=True
-               , xlabel="Freq", ylabel="Ch", title="Plot"
-               , reverse=True)
+# customplot(f[0].transpose(), save=True, show=True, filename="plot.html"
+#                , w=300, h=200, t=np.linspace(4, 36, 300)
+#                , y=np.linspace(1, 16, 200), relative=True
+#                , xlabel="Freq", ylabel="Ch", title="Plot"
+#                , reverse=True)
 
-f = mne.time_frequency.psd_multitaper(raw, fmin=32, fmax=90, tmin=None, tmax=None,
-                                  bandwidth=2, adaptive=False, low_bias=True,
-                                  normalization='length', picks=ch_names, proj=False,
-                                  n_jobs=1, verbose=None)
-
-
-customplot(f[0].transpose(), save=True, show=True, filename="plot.html"
-               , w=300, h=200, t=np.linspace(32, 90, 300)
-               , y=np.linspace(1, 16, 200), relative=True
-               , xlabel="Freq", ylabel="Ch", title="Plot"
-               , reverse=True)
-
-f = mne.time_frequency.psd_multitaper(raw, fmin=4, fmax=90, tmin=None, tmax=None,
-                                  bandwidth=2, adaptive=False, low_bias=True,
-                                  normalization='length', picks=ch_names, proj=False,
-                                  n_jobs=1, verbose=None)
-
-customplot(f[0].transpose(), save=True, show=True, filename="plot.html"
-               , w=300, h=200, t=np.linspace(4, 90, 300)
-               , y=np.linspace(1, 16, 200), relative=True
-               , xlabel="Freq", ylabel="Ch", title="Plot"
-               , reverse=True)
-
+### Event related potential and current source density plots
 # ERP_plot(save=True, data=data, key="pfc")
 # CSD_plot(save=True, data=data, key='pfc')
 
-# psp_plotter(data=data, key='pfc', save=True)
+### Power spectrum (multitaper) plots on heatmap
+# psp_plotter(data=data, key='pfc', save=True, t1=500, t2=2500, fmin=0,
+#             fmax=100, normalize_w=False, bw=45, k=0)
+# psp_plotter(data=data, key='pfc', save=True, t1=2500, t2=4500, fmin=0,
+#             fmax=100, normalize_w=False, bw=45, k=0)
+
+### Channelwise spectral coherence
+# coherence_plotter(data=data, key='pfc', save=True, t1=500, t2=2500, fmin=4,
+#             fmax=100, normalize_w=True, bw=45, k=0)
+# coherence_plotter(data=data, key='pfc', save=True, t1=2500, t2=4500, fmin=4,
+#             fmax=100, normalize_w=True, bw=45, k=0)
+
+
