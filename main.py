8cd1a5f63a970ab8ab291d4ffcd880d0d4c429bd
Hamed Nejat <59973245+HNXJ@users.noreply.github.com>
2021-10-06 01:33:31 +0330

    V1.4 Single trial added

--- main.py
+++ main.py
@@ -2,23 +2,29 @@ import scipy.io as sio
 from Methods import *
 
 ### Load data and show content of it
-data = sio.loadmat('data.mat') # Example data, private access
-print_all_content(data)
+# data = sio.loadmat('data.mat') # Example data, private access
+# print_all_content(data)
 
 ### Event related potential and current source density plots
-ERP_plot(save=True, data=data, key="pfc")
-CSD_plot(save=True, data=data, key='pfc')
+# ERP_plot(save=True, data=data, key="pfc")
+# CSD_plot(save=True, data=data, key='pfc')
 
 ### Power spectrum (multitaper) plots on heatmap
 psp_plotter(data=data, key='pfc', save=True, t1=500, t2=2500, fmin=0,
-            fmax=100, normalize_w=False, bw=45, k=0)
-psp_plotter(data=data, key='pfc', save=True, t1=2500, t2=4500, fmin=0,
-            fmax=100, normalize_w=True, bw=45, k=0)
+            fmax=100, normalize_w=True, bw=45, k=0, title="PS, all trials ")
+# psp_plotter(data=data, key='pfc', save=True, t1=2500, t2=4500, fmin=0,
+#             fmax=100, normalize_w=True, bw=45, k=0)
 
 ### Channelwise spectral coherence
-coherence_plotter(data=data, key='pfc', save=True, t1=500, t2=2500, fmin=4,
-            fmax=100, normalize_w=True, bw=45, k=0)
-coherence_plotter(data=data, key='pfc', save=True, t1=2500, t2=4500, fmin=4,
-            fmax=100, normalize_w=True, bw=45, k=0)
+# coherence_plotter(data=data, key='pfc', save=True, t1=500, t2=2500, fmin=4,
+#             fmax=100, normalize_w=True, bw=45, k=0)
+# coherence_plotter(data=data, key='pfc', save=True, t1=2500, t2=4500, fmin=4,
+#             fmax=100, normalize_w=True, bw=45, k=0)
 
-
+### Single trial parts
+psp_plotter(data=data, key='pfc', save=True, t1=2500, t2=4500, fmin=0,
+            fmax=100, normalize_w=True, bw=45, k=0, trials=[100],
+            title="PS, trial 100")
+psp_plotter(data=data, key='pfc', save=True, t1=2500, t2=4500, fmin=0,
+            fmax=100, normalize_w=True, bw=45, k=0, trials=[400],
+            title="PS, trial 400")
