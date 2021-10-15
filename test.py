cc4245ee278bee6128fda632e9b50cfc79cd5378
HamedNejat <59973245+HNXJ@users.noreply.github.com>
2021-10-15 17:17:52 +0330

    LT V.2

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
-import os.path as op
+import scipy.io as sio
+from Methods import *
+from Granger import *
 
-import numpy as np
-import matplotlib.pyplot as plt
-
-import mne
-from mne.time_frequency import tfr_morlet, psd_multitaper, psd_welch
-from mne.datasets import somato
+### Load data and show content of it
+data = sio.loadmat('data.mat') # Example data, private access
+print_all_content(data)
 
 
-mne.time_frequency.psd_multitaper(inst, fmin=0, fmax=inf, tmin=None, tmax=None, bandwidth=None, adaptive=False, low_bias=True, normalization='length', picks=None, proj=False, n_jobs=1, reject_by_annotation=False, verbose=None)
