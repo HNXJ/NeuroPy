8844c1f17d7a3691041e8eae0eb35d8b19830b03
HamedNejat <59973245+HNXJ@users.noreply.github.com>
2021-10-16 17:06:12 +0330

    V2.2

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ from Methods import *
 from Granger import *
 
 ### Load data and show content of it
-data = sio.loadmat('data.mat') # Example data, private access
+# data = sio.loadmat('data.mat') # Example data, private access
+# cues = sio.loadmat('cues.mat') # Example data's cue tye array
+cues = cues['c']
 print_all_content(data)
-
 
