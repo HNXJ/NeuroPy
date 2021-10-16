2c398b62183eea00ae848e0a44d5b525f38b00bc
HamedNejat <59973245+HNXJ@users.noreply.github.com>
2021-10-16 17:05:37 +0330

    V2.2

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ from Methods import *
 from Granger import *
 
 ### Load data and show content of it
-data = sio.loadmat('data.mat') # Example data, private access
-print_all_content(data)
+# data = sio.loadmat('data.mat') # Example data, private access
+# cues = sio.loadmat('cues.mat') # Example data's cue tye array
+# cues = cues['c']
+# print_all_content(data)
+
+### Trial ID decomposition
+t_exp = get_trials(cues, mode='block', l=0, r=100)
+t_unx = get_trials(cues, mode='trial', l=0, r=100)
+
 
 ### Event related potential and current source density plots
 # ERP_plot(save=True, data=data, key="pfc")
@@ -25,56 +32,26 @@ print_all_content(data)
 ### Selective or single trial parts: PSP and COH
 ### PSP
 # psp_plotter(data=data, key='pfc', save=True, t1=2500, t2=4500, fmin=0,
-#             fmax=100, normalize_w=True, bw=45, k=0, trials=[100],
-#             title="PS, trial 100")
-# psp_plotter(data=data, key='pfc', save=True, t1=2500, t2=4500, fmin=0,
-#             fmax=100, normalize_w=True, bw=45, k=0, trials=[400],
-#             title="PS, trial 400")
+#             fmax=100, normalize_w=True, bw=45, k=0, trials=t_exp,
+#             title="PS, block mode (expected)")
 
-### COH
+# psp_plotter(data=data, key='pfc', save=True, t1=2500, t2=4500, fmin=0,
+#             fmax=100, normalize_w=True, bw=45, k=0, trials=t_unx,
+            # title="PS, trial mode (unexpected)")
+
+### Coherence (spectral)
 # coherence_plotter(data=data, key='pfc', save=True, t1=500, t2=2500, fmin=4,
 #             fmax=100, normalize_w=True, bw=45, k=0,
 #             title="Coherence, trial 100, 200", trials=[100, 200])
-# coherence_plotter(data=data, key='pfc', save=True, t1=2500, t2=4500, fmin=4,
-#             fmax=100, normalize_w=True, bw=45, k=0,
-#             title="Coherence, trial 400, 500", trials=[400, 500])
 
-# granger_plotter(data=data, key='pfc', save=True, t1=500, t2=2500, fmin=4,
-#             fmax=100, normalize_w=True, bw=45, k=0,
-#             title="Granger causality (maxlag=12 min P-values) on trials 1-100",
-#             trials=[i for i in range(100)], lag=5)
+### Coherence (granger)
+s_granger_plotter(data=data, key='pfc', save=True, t1=100, t2=2500, fmin=4,
+            fmax=100, normalize_w=True, bw=45, k=0,
+            title="Granger causality (maxlag=6 min P-values) on block (exp) first 100 trials",
+            trials=t_exp, lag=6)
 
-# granger_plotter(data=data, key='pfc', save=True, t1=500, t2=2500, fmin=4,
-#             fmax=100, normalize_w=True, bw=45, k=0,
-#             title="Granger causality (maxlag=12 min P-values) on trials 501-600",
-#             trials=[i for i in range(500, 600)], lag=5)
+s_granger_plotter(data=data, key='pfc', save=True, t1=100, t2=2500, fmin=4,
+            fmax=100, normalize_w=True, bw=45, k=0,
+            title="Granger causality (maxlag=6 min P-values) on trial (unexp) first 100 trials",
+            trials=t_unx, lag=6)
 
-# granger_plotter(data=data, key='pfc', save=True, t1=500, t2=2500, fmin=4,
-#             fmax=100, normalize_w=True, bw=45, k=0,
-#             title="Granger causality (maxlag=12 min P-values) on trial 100",
-#             trials=[100], lag=5)
-
-# granger_plotter(data=data, key='pfc', save=True, t1=500, t2=2500, fmin=4,
-#             fmax=100, normalize_w=True, bw=45, k=0,
-#             title="Granger causality (maxlag=12 min P-values) on trial 200",
-#             trials=[200], lag=5)
-
-# granger_plotter(data=data, key='pfc', save=True, t1=500, t2=2500, fmin=4,
-#             fmax=100, normalize_w=True, bw=45, k=0,
-#             title="Granger causality (maxlag=12 min P-values) on trial 300",
-#             trials=[300], lag=5)
-
-# granger_plotter(data=data, key='pfc', save=True, t1=500, t2=2500, fmin=4,
-#             fmax=100, normalize_w=True, bw=45, k=0,
-#             title="Granger causality (maxlag=12 min P-values) on trial 400",
-#             trials=[400], lag=5)
-
-# granger_plotter(data=data, key='pfc', save=True, t1=500, t2=2500, fmin=4,
-#             fmax=100, normalize_w=True, bw=45, k=0,
-#             title="Granger causality (maxlag=12 min P-values) on trial 500",
-#             trials=[500], lag=5)
-
-# granger_plotter(data=data, key='pfc', save=True, t1=500, t2=2500, fmin=4,
-#             fmax=100, normalize_w=True, bw=45, k=0,
-#             title="Granger causality (maxlag=12 min P-values) on trial 600",
-#             trials=[599], lag=5)
