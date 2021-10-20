1ed4e20e5b292cde8ad92029b2ca7e0124be44c9
HamedNejat <59973245+HNXJ@users.noreply.github.com>
2021-10-20 05:28:17 +0330

    V7.0

--- test.py
+++ test.py
@@ -5,6 +5,15 @@ from Granger import *
 ### Load data and show content of it
 # data = sio.loadmat('data.mat') # Example data, private access
 # cues = sio.loadmat('cues.mat') # Example data's cue tye array
-cues = cues['c']
-print_all_content(data)
+# cues = cues['c']
+# print_all_content(data)
 
+# ### Trial ID decomposition
+# t_exp = get_trials(cues, mode='block', l=0, r=100)
+# t_unx = get_trials(cues, mode='trial', l=0, r=100)
+
+psd, freqs = power_spectrum_density(data=data, key='pfc', save=True, t1=2500, t2=4500, fmin=0,
+            fmax=100, normalize_w=True, bw=45, k=0, trials=t_exp)
+
+
+pink_noise_inverse_filter(x=, w=5)
