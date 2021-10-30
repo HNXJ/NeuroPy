65b79e27125ed1d048d98175c3c09a03fd3a276e
Hamed Nejat <hamednejat7@gmail.com>
2021-10-30 04:39:37 +0330

    V7.8.1

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import Viewer
 import numpy as np
 
 
-# dataset = Datasets.Dataset()
-# dataset.load_laminar_data(filename="Data/data.mat")
-# dataset.print_all_content()
-# trials_block = dataset.get_trials(key='block', l=0, r=600)
-# trials_trial = dataset.get_trials(key='trial', l=0, r=600)
-# trials = [i for i in range(0, 600, 1)]
+dataset = Datasets.Dataset()
+dataset.load_laminar_data(filename="Data/data.mat")
+dataset.print_all_content()
+trials_block = dataset.get_trials(key='block', l=0, r=600)
+trials_trial = dataset.get_trials(key='trial', l=0, r=600)
+trials = [i for i in range(0, 600, 1)]
 
 ##############################################################################
 
