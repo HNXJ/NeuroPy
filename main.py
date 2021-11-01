409aabe436faf74f59ecd39f9e529cdd5d95a821
Hamed Nejat <hamednejat7@gmail.com>
2021-11-01 05:26:00 +0330

    V8.0.0

--- main.py
+++ main.py
@@ -16,19 +16,19 @@ warnings.filterwarnings("ignore")
 # dataset.print_all_content()
 # trials_block = dataset.get_trials(key='block', l=0, r=600)
 # trials_trial = dataset.get_trials(key='trial', l=0, r=600)
-trials = [i for i in range(140, 360, 1)]
+# trials = [i for i in range(0, 600, 1)]
 
 ##############################################################################
 
-[tpsd, freqs, times] = Datasets.load_list("Data/1-600-tpsd-250ms.txt")
+# [tpsd, freqs, times] = Datasets.load_list("Data/1-600-tpsd-250ms.txt")
 
-dim = 3
-y = (np.array(trials)//50)%2
-x = tpsd[4:5, 3:15, 2:6, trials].reshape([-1, len(trials)]).transpose()
-X = Learning.tsne_cluster(X=x, Y=y, components=dim, perplx=7,
-                                   learning_rate=400, visualize=True,
-                                   iterations=3000, tit="tSNE-it6000-px180-lr100",
-                                   save=True, name="plot")
+# dim = 3
+# y = (np.array(trials)//50)%2
+# x = tpsd[7:9, 3:15, 2:6, trials].reshape([-1, len(trials)]).transpose()
+# X = Learning.tsne_cluster(X=x, Y=y, components=dim, perplx=10,
+#                                    learning_rate=200, visualize=True,
+#                                    iterations=6000, tit="tSNE-it6000-px180-lr100",
+#                                    save=True, name="plot")
     
 # Viewer.scatter(data=X, y=y, dim=dim, frames=1, title="TSNE cluster in time",
 #                xlabel="", ylabel="", fr=times, trials=trials, bands=False)
@@ -48,12 +48,12 @@ X = Learning.tsne_cluster(X=x, Y=y, components=dim, perplx=7,
 # trials = [i for i in range(100, 300, 1)]
 
 ### GC is time windows
-# tgc, times = Connect.time_granger_causality(data=dataset.signals['pfc'],
-#                                         time_window_size=250, time_overlap=0,
-#                                         trials=trials, bw=50, tl=500, tr=3500,
-#                                         time_base=-1000)
+tgc, times = Connect.time_granger_causality(data=dataset.signals['pfc'],
+                                        time_window_size=250, time_overlap=0,
+                                        trials=trials, bw=50, tl=0, tr=4000,
+                                        time_base=-1500)
 
-# Methods.save_list([tgc, times], "Data/1-600-tgc-250ms.txt")
+Methods.save_list([tgc, times], "Data/1-600-tgc-250ms.txt")
 
 # tpsd[np.isnan(tpsd)] = 0
 # for i in range(600):
@@ -62,7 +62,9 @@ X = Learning.tsne_cluster(X=x, Y=y, components=dim, perplx=7,
     
 # y = (np.array(trials)//50)%2
 
-# Connect.time_tsne_cluster(data=tpsd[:, 4:15, 2:6, :], y=y, trials=trials, dim=3, perplx=120, learning_rate=70, 
+
+# [tgc, times] = Datasets.load_list("Data/1-600-tgc.txt")
+# Connect.time_tsne_cluster(data=tpsd[:, 3:16, 3:16, :], y=y, trials=trials, dim=3, perplx=120, learning_rate=70, 
 #                       n_iter=5000, times=times, title="tSNE in time", name="TtSNE3D")
 
 ### Best tSNE/PCAs
