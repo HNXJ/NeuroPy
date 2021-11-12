c3be907160354e81fb945a97795c0f277ac2d24d
Hamed Nejat <hamednejat7@gmail.com>
2021-11-12 17:20:45 +0330

    V8.0.8

--- main.py
+++ main.py
@@ -18,55 +18,55 @@ warnings.filterwarnings("ignore")
 ##############################################################################
 
 
-dataset = Datasets.Dataset()
-dataset.load_laminar_data(path="Data/")
-dataset.print_all_content()
-trials_block = dataset.get_trials(key='block', l=0, r=600)
-trials_trial = dataset.get_trials(key='trial', l=0, r=600)
-trials = [i for i in range(0, 600, 1)]
+# dataset = Datasets.Dataset()
+# dataset.load_laminar_data(path="Data/")
+# dataset.print_all_content()
+# trials_block = dataset.get_trials(key='block', l=0, r=600)
+# trials_trial = dataset.get_trials(key='trial', l=0, r=600)
+# trials = [i for i in range(0, 600, 1)]
 
 
 ##############################################################################
 
 
-# [tpsd, freqs, times] = Datasets.load_list("Data/1-600-tpsd-500ms.txt")
+[tpsd, freqs, times] = Datasets.load_list("Data/1-600-tpsd-500ms.txt")
 
-# for i in range(tpsd.shape[0]):
-#     for j in range(tpsd.shape[3]):
-#         tpsd[i, :, :, j] /= np.max(np.max(tpsd[i, :, :, j])) + 0.0001
-#         tpsd[i, :, :, j] = np.sqrt(tpsd[i, :, :, j])
+for i in range(tpsd.shape[0]):
+    for j in range(tpsd.shape[3]):
+        tpsd[i, :, :, j] /= np.max(np.max(tpsd[i, :, :, j])) + 0.0001
+        tpsd[i, :, :, j] = np.sqrt(tpsd[i, :, :, j])
 
-# trials = [i for i in range(120, 380)]
+trials = [i for i in range(120, 380)]
 
-# dim = 3
-# y = (np.array(trials)//50)%2
-# yl = []
+dim = 3
+y = (np.array(trials)//50)%2
+yl = []
 
-# for i in range(len(trials)):
-#     if y[i] == 1:
-#         yl.append("Trial(unpredictable)")
-#     elif y[i] == 0:
-        # yl.append("Block(predictable)")
+for i in range(len(trials)):
+    if y[i] == 1:
+        yl.append("Trial(unpredictable)")
+    elif y[i] == 0:
+        yl.append("Block(predictable)")
         
-# for i in range(len(trials)):
-#     if y[i] == 1:
-#         yl.append("cor")
-#     elif y[i] == 0:
-#         yl.append("err")
+for i in range(len(trials)):
+    if y[i] == 1:
+        yl.append("cor")
+    elif y[i] == 0:
+        yl.append("err")
         
 # y = np.reshape(dataset.cue_cr[trials], [-1])*3 #+ 5 * ((np.array(trials)//50)%2)
 
-# x = tpsd[:, 9:16, 2:7, trials].reshape([-1, len(trials)*tpsd.shape[0]]).transpose()
+x = tpsd[:, 9:16, 2:7, trials].reshape([-1, len(trials)*tpsd.shape[0]]).transpose()
 
 # Connect.time_tsne_cluster(data=tpsd[:, 10:16, 3:7, :], y=yl, trials=trials,
 #                           dim=3, perplx=20, learning_rate=25, 
 #                           n_iter=6000, times=times, title="tSNE in time for PSD",
 #                           name="TtSNE3DGC", ee=15, method="exact")
 
-# X = Learning.tsne_cluster(X=x, Y=y, components=dim, perplx=5,
-#                                     learning_rate=25, visualize=True,
-#                                     iterations=5000, tit="tSNE-it6000-px180-lr100",
-#                                     save=True, name="plot", ee=10, init='pca')#, method="exact")
+X = Learning.tsne_cluster(X=x, Y=y, components=dim, perplx=5,
+                                    learning_rate=25, visualize=True,
+                                    iterations=5000, tit="tSNE-it6000-px180-lr100",
+                                    save=True, name="plot", ee=10, init='pca')#, method="exact")
     
 # X = Learning.pca_cluster(X=x, Y=y, components=dim, visualize=True, tit="PFC-PSD-PCA"
 #             , save=True, name="pcapfcpsd")
