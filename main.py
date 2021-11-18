2d622b58f389539d70cc44a5117f2a33fee92ef5
Hamed Nejat <hamednejat7@gmail.com>
2021-11-18 14:38:39 +0330

    V8.1.4

--- main.py
+++ main.py
@@ -22,14 +22,14 @@ warnings.filterwarnings("ignore")
 
 ### TODO: RSA/RDM test
 
-trials = [i for i in range(0, 100, 1)]
-[tpsd, freqs, times] = Datasets.load_list("Data/1-600-tpsd-1000ms.txt")
-[tsc] = Datasets.load_list("Data/1-600-tsc-1000ms.txt")
-[tgc, times] = Datasets.load_list("Data/1-600-tgc-250ms.txt")
+# trials = [i for i in range(0, 100, 1)]
+# [tpsd, freqs, times] = Datasets.load_list("Data/1-600-tpsd-1000ms.txt")
+# [tsc] = Datasets.load_list("Data/1-600-tsc-1000ms.txt")
+# [tgc, times] = Datasets.load_list("Data/1-600-tgc-250ms.txt")
 
-rdm_ = Representational.time_rdm(x=tsc, p_dim=3, t_dim=0, trials=trials)
+# rdm_ = Representational.time_rdm(x=tsc, p_dim=3, t_dim=0, trials=trials)
 
-Representational.time_rdm_plot(rdm_, title="RDM", dlabel="Trials", times=times)
+# Representational.time_rdm_plot(rdm_, title="RDM", dlabel="Trials", times=times)
 
 
 ##############################################################################
