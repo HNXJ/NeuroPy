44fc7684c777a7db8f0f80078a2fe47cc48966c7
Hamed Nejat <hamednejat7@gmail.com>
2021-11-20 10:13:43 +0330

    V8.1.5

--- testbench.py
+++ testbench.py
@@ -22,55 +22,53 @@ warnings.filterwarnings("ignore")
 # dataset.print_all_content()
 # trials_block = dataset.get_trials(key='block', l=0, r=600)
 # trials_trial = dataset.get_trials(key='trial', l=0, r=600)
-# trials = [i for i in range(0, 600, 1)]
+trials = [i for i in range(0, 600, 1)]
 
 ##############################################################################
 
-# sample_inds = []
-# for i in range(6):
-#     sample_inds.append([])
+sample_inds = []
+for i in range(6):
+    sample_inds.append([])
 
-# for i in trials:
-#     if dataset.cue_s[i] == 1 and dataset.cues[i] == 0: # pred & A
-#         sample_inds[0].append(i)
-#     if dataset.cue_s[i] == 2 and dataset.cues[i] == 0: # pred & B
-#         sample_inds[1].append(i)
+for i in trials:
+    if dataset.cue_s[i] == 1 and dataset.cues[i] == 0: # pred & A
+        sample_inds[0].append(i)
+    if dataset.cue_s[i] == 2 and dataset.cues[i] == 0: # pred & B
+        sample_inds[1].append(i)
         
-#     if dataset.cue_s[i] == 3 and dataset.cues[i] == 0: # pred & C
-#         sample_inds[2].append(i)
-#     if dataset.cue_s[i] == 1 and dataset.cues[i] == 1: # unpred & A
-#         sample_inds[3].append(i)
+    if dataset.cue_s[i] == 3 and dataset.cues[i] == 0: # pred & C
+        sample_inds[2].append(i)
+    if dataset.cue_s[i] == 1 and dataset.cues[i] == 1: # unpred & A
+        sample_inds[3].append(i)
         
-#     if dataset.cue_s[i] == 2 and dataset.cues[i] == 1: # unpred & B
-#         sample_inds[4].append(i)
-#     if dataset.cue_s[i] == 3 and dataset.cues[i] == 1: # unpred & C
-#         sample_inds[5].append(i)
+    if dataset.cue_s[i] == 2 and dataset.cues[i] == 1: # unpred & B
+        sample_inds[4].append(i)
+    if dataset.cue_s[i] == 3 and dataset.cues[i] == 1: # unpred & C
+        sample_inds[5].append(i)
      
          
 ### RSA/RDM test 2X3 -> 6 class 
 
-# trials = [i for i in range(0, 100, 1)]
-# [tpsd, freqs, times] = Datasets.load_list("Data/1-600-tpsd-500ms.txt")
-# [tsc] = Datasets.load_list("Data/1-600-tsc-500ms.txt")
-# # [tgc, times] = Datasets.load_list("Data/1-600-tgc-250ms.txt")
+trials = [i for i in range(0, 100, 1)]
+[tpsd, freqs, times] = Datasets.load_list("Data/1-600-tpsd-500ms-f3-f247-pfc.txt")
+tpsd[np.isnan(tpsd)] = 0
+xt_array = tpsd
 
-# # xt_array = tpsd
+xt = np.zeros([xt_array.shape[0], xt_array.shape[1], xt_array.shape[2], 60])
+for i in range(len(sample_inds)):
+    xt[:, :, :, i*10:(i+1)*10] = xt_array[:, :, :, sample_inds[i][:10]]
 
-# # xt = np.zeros([xt_array.shape[0], xt_array.shape[1], xt_array.shape[2], 6])
-# # for i in range(len(sample_inds)):
-# #     xt[:, :, :, i] = np.mean(xt_array[:, :, :, sample_inds[i]], 3)
+Datasets.save_list(xt, "xt.txt")
+xt = Datasets.load_list("xt.txt")
 
-# # Datasets.save_list(xt, "xt.txt")
-# xt = Datasets.load_list("xt.txt")
+rdm_ = Representational.time_rdm(x=xt[:, :, 10:11, :], p_dim=3, t_dim=0, trials=[i for i in range(6)])
 
-# rdm_ = Representational.time_rdm(x=xt, p_dim=3, t_dim=0, trials=[i for i in range(6)])
+ctl = ["A-Pred", "B-Pred", "C-Pred", "A-Unpred", "B-Unpred", "C-Unpred"]
 
-# ctl = ["A-Pred", "B-Pred", "C-Pred", "A-Unpred", "B-Unpred", "C-Unpred"]
+app = Representational.time_rdm_plot(rdm_, title="RDM, average across temp-spect-coherence categories, Each time frame"
+                                , dlabel="Modes", times=times, cat_labels=ctl)
 
-# app = Representational.time_rdm_plot(rdm_, title="RDM, average across temp-spect-coherence categories, Each time frame"
-#                                , dlabel="Modes", times=times, cat_labels=ctl)
-
-# app.run_server()
+app.run_server()
 
 
 # server = Flask(__name__)
@@ -82,7 +80,7 @@ warnings.filterwarnings("ignore")
 
 trials = [i for i in range(0, 600)]
 
-# ### PSD in time windows till tSNE
+### PSD in time windows till tSNE
 # tpsd, freqs, times = Connect.time_power_spectrum_density(data=dataset.signals['pfc']
 #                                 , save=True, bands=True
 #                                 , time_window_size=500, time_overlap=0
@@ -112,29 +110,29 @@ trials = [i for i in range(0, 600)]
 
 # [tpsd, freqs, times] = Datasets.load_list("Data/1-600-tpsd-500ms-v4.txt")
 
-for i in range(tpsd.shape[0]):
-    for j in range(tpsd.shape[3]):
-        tpsd[i, :, :, j] /= np.max(np.max(tpsd[i, :, :, j])) + 0.0001
-        tpsd[i, :, :, j] = np.sqrt(tpsd[i, :, :, j])
+# for i in range(tpsd.shape[0]):
+#     for j in range(tpsd.shape[3]):
+#         tpsd[i, :, :, j] /= np.max(np.max(tpsd[i, :, :, j])) + 0.0001
+#         tpsd[i, :, :, j] = np.sqrt(tpsd[i, :, :, j])
 
-trials = [i for i in range(130, 270)]
+# trials = [i for i in range(130, 270)]
 
-dim = 3
-# y = (np.array(trials)//50)%2
-yl = []
-y = np.reshape(dataset.cue_s[trials], [-1]) #+ 5 * ((np.array(trials)//50)%2)
+# dim = 3
+# # y = (np.array(trials)//50)%2
+# yl = []
+# y = np.reshape(dataset.cue_s[trials], [-1]) #+ 5 * ((np.array(trials)//50)%2)
 
-for i in range(len(trials)):
-    if y[i] == 1:
-        yl.append("Trial(unpredictable)")
-    elif y[i] == 0:
-        yl.append("Block(predictable)")
+# for i in range(len(trials)):
+#     if y[i] == 1:
+#         yl.append("Trial(unpredictable)")
+#     elif y[i] == 0:
+#         yl.append("Block(predictable)")
         
-for i in range(len(trials)):
-    if y[i] == 1:
-        yl.append("cor")
-    elif y[i] == 0:
-        yl.append("err")
+# for i in range(len(trials)):
+#     if y[i] == 1:
+#         yl.append("cor")
+#     elif y[i] == 0:
+#         yl.append("err")
         
 # for i in range(len(trials)):
 #     if y[i] == 1:
